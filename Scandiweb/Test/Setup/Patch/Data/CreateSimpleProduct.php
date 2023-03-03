bf5321b5acb4f32ae0767bcc7b84ff7da53dc177
Abdelrahman Gira <abdelrahman.gira@scandiweb.com>
2023-03-03 18:09:06 +0200

    Test Module created

---
+++ Scandiweb/Test/Setup/Patch/Data/CreateSimpleProduct.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Scandiweb\Test\Setup\Patch\Data;
+
+use \Magento\Catalog\Api\CategoryLinkManagementInterface;
+use \Magento\Catalog\Api\Data\ProductInterfaceFactory;
+use \Magento\Catalog\Api\ProductRepositoryInterface;
+use \Magento\Catalog\Model\Product;
+use \Magento\Catalog\Model\Product\Attribute\Source\Status;
+use \Magento\Catalog\Model\Product\Type;
+use \Magento\Catalog\Model\Product\Visibility;
+use \Magento\Eav\Setup\EavSetup;
+use \Magento\Framework\App\State;
+use \Magento\Framework\Setup\ModuleDataSetupInterface;
+use \Magento\Framework\Setup\Patch\DataPatchInterface;
+use \Magento\Store\Model\StoreManagerInterface;
+use \Magento\InventoryApi\Api\Data\SourceItemInterface;
+use \Magento\InventoryApi\Api\Data\SourceItemInterfaceFactory;
+use \Magento\InventoryApi\Api\SourceItemsSaveInterface;
+
+class CreateSimpleProduct Implements \Magento\Framework\Setup\Patch\DataPatchInterface {
+
+    protected ModuleDataSetupInterface $setup;
+
+    protected ProductInterfaceFactory $productInterfaceFactory;
+
+    protected ProductRepositoryInterface $productRepository;
+
+    protected State $appState;
+
+	protected EavSetup $eavSetup;
+
+    protected StoreManagerInterface $storeManager;
+
+    protected SourceItemInterfaceFactory $sourceItemFactory;
+
+    protected SourceItemsSaveInterface $sourceItemsSaveInterface;
+
+    protected CategoryLinkManagementInterface $categoryLink;
+
+    protected array $sourceItems = [];
+
+    public function __construct(
+        ModuleDataSetupInterface $setup,
+        ProductInterfaceFactory $productInterfaceFactory,
+        ProductRepositoryInterface $productRepository,
+        State $appState,
+        StoreManagerInterface $storeManager,
+        EavSetup $eavSetup,
+		SourceItemInterfaceFactory $sourceItemFactory,
+        SourceItemsSaveInterface $sourceItemsSaveInterface,
+		CategoryLinkManagementInterface $categoryLink
+    ) {
+        $this->appState = $appState;
+        $this->productInterfaceFactory = $productInterfaceFactory;
+        $this->productRepository = $productRepository;
+        $this->setup = $setup;
+		$this->eavSetup = $eavSetup;
+        $this->storeManager = $storeManager;
+        $this->sourceItemFactory = $sourceItemFactory;
+        $this->sourceItemsSaveInterface = $sourceItemsSaveInterface;
+		$this->categoryLink = $categoryLink;
+    }
+
+    public function apply()
+    {
+        $this->appState->emulateAreaCode('adminhtml', [$this, 'execute']);
+    }
+
+    public function execute()
+    {
+        $product = $this->productInterfaceFactory->create();
+
+        if ($product->getIdBySku('grip-trainer')) {
+            return;
+        }
+
+        $attributeSetId = $this->eavSetup->getAttributeSetId(Product::ENTITY, 'Default');
+        $product->setTypeId(Type::TYPE_SIMPLE)
+            ->setAttributeSetId($attributeSetId)
+            ->setName('Grip Trainer')
+            ->setSku('grip-trainer')
+            ->setUrlKey('griptrainer')
+            ->setPrice(9.99)
+            ->setVisibility(Visibility::VISIBILITY_BOTH)
+            ->setStatus(Status::STATUS_ENABLED);
+
+        $product = $this->productRepository->save($product);
+        $this->categoryLink->assignProductToCategories($product->getSku(), [2]);
+    }
+
+    public function getAliases() {
+        return [];
+    }
+    public static function getDependencies() {
+        return [];
+    }
+    public function revert() {}
+    public function getVersion() {}
+}
---
+++ Scandiweb/Test/Test.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Code\Scandiweb\Test;
+
+
+class Test {
+    
+}
